cb1a3f7e7cef3529ef075729681da55369192d55
NguyenDinhHuyDe190538 <dinhhuy8024@gmail.com>
2025-10-21 15:28:15 +0700

    Add files via upload

---
+++ lab6/Lab211_W6_Bai1/src/lab211_w6_bai1/FruitShop.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab211_w6_bai1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author dinhh
+ */
+public class FruitShop {
+
+    private List<Fruit> fruits = new ArrayList<>();
+    private List<Order> orders = new ArrayList<>();
+
+    public void run() {
+        addFruits();
+        showFruits();
+        shopping();
+        viewOrder();
+
+    }
+
+    private void addFruits() {
+        fruits.add(new Fruit("01", "banana", 3.0, 12, "VN"));
+        fruits.add(new Fruit("02", "orange", 3.5, 120, "VN"));
+        fruits.add(new Fruit("03", "apple", 1.5, 100, "VN"));
+    }
+
+    public void showFruits() {
+        System.out.println("| Item | Fruit Name | Origin | Price | Quantity");
+        int i = 1;
+        for (Fruit f : fruits) {
+            System.out.printf("%-5d | %-10s | %-6s | %6.2f$ | %5d\n",
+                    i++, f.getName(), f.getOrigin(), f.getPrice(), f.getQuantity());
+        }
+    }
+
+    private void shopping() {
+        Order order1 = new Order("Hung"); // khach 1
+        List<OrderDetail> bag = new ArrayList<>();
+
+        Fruit item1 = fruits.get(1); // lay item 1-2 
+        int qty1 = 10;
+        item1.setQuantity(item1.getQuantity() - qty1);
+        bag.add(new OrderDetail(item1, qty1));
+
+        Fruit item2 = fruits.get(2);
+        int qty2 = 10;
+        item2.setQuantity(item2.getQuantity() - qty2);
+        bag.add(new OrderDetail(item2, qty2));
+
+        for (OrderDetail od : bag) {
+            order1.addDetail(od);
+        }
+        orders.add(order1);
+        //lam them 1 khach hang mua nua??
+    }
+
+    private void viewOrder() {
+        System.out.println("danh sach chi tiet mua hang");
+        for (Order o : orders) {
+            System.out.println(o);
+        }
+    }
+
+}
---
+++ lab6/Lab211_W6_Bai1/src/lab211_w6_bai1/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab211_w6_bai1;
+
+/**
+ *
+ * @author dinhh
+ */
+public class Main {
+    public static void main(String[] args) {
+        FruitShop shop = new FruitShop();
+        shop.run();
+    }
+}
